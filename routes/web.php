9860d38d43739a12b06d82e16edcb6ca1237de62
Jacob Hammerle <hammerja@miamioh.edu>
2017-05-29 15:28:37 -0400

    copying over code

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/* ROUTE TO ABOUT PAGE */
+Route::get('contact', function () {
+    return view('contact');
+});
+
+/* ROUTE TO ABOUT PAGE */
+Route::get('about', function () {
+    return view('about');
+});
+
+/* ROUTE TO INDEX PAGE */
+Route::get('/', function () {
+    return view('welcome');
+});
