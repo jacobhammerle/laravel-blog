1c8b5edb15cb93919a76c4f043f850aa52905c26
Jacob Hammerle <hammerja@miamioh.edu>
2017-06-01 19:14:34 -0400

    post html/css structure and sidebar

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,14 +1,53 @@
 @extends('main')
 
 @section('content')
-    <div class="content">
-        <div class="title m-b-md">
-            <h1>Welcome</h1>
-        </div> 
-        <div class="links">
-            <a href="/">Home</a>
-            <a href="about">About</a>
-            <a href="contact">Contact</a>
+    <div class="row">
+        <div class="col m8 s12">
+            <div class="post-block">
+                <div class="post-content">
+                    <h4>Post Title</h4>
+                    <p>Lorem ipsum dolor sit amet, facilis volutpat ex est, te probatus adipisci eum. Epicuri antiopam mei an. Mea ei alii eruditi, consul qualisque vix an, pro at deleniti vituperatoribus. No nec lorem patrioque, mel ea agam mucius alienum.</p>
+                    <span>Read more</span>
+                </div>
+            </div>
+            <div class="post-block">
+                <div class="post-content">
+                    <h4>Post Title</h4>
+                    <p>Lorem ipsum dolor sit amet, facilis volutpat ex est, te probatus adipisci eum. Epicuri antiopam mei an. Mea ei alii eruditi, consul qualisque vix an, pro at deleniti vituperatoribus. No nec lorem patrioque, mel ea agam mucius alienum.</p>
+                    <span>Read more</span>
+                </div>
+            </div>
+            <div class="post-block">
+                <div class="post-content">
+                    <h4>Post Title</h4>
+                    <p>Lorem ipsum dolor sit amet, facilis volutpat ex est, te probatus adipisci eum. Epicuri antiopam mei an. Mea ei alii eruditi, consul qualisque vix an, pro at deleniti vituperatoribus. No nec lorem patrioque, mel ea agam mucius alienum.</p>
+                    <span>Read more</span>
+                </div>
+            </div>
+            <div class="post-block">
+                <div class="post-content">
+                    <h4>Post Title</h4>
+                    <p>Lorem ipsum dolor sit amet, facilis volutpat ex est, te probatus adipisci eum. Epicuri antiopam mei an. Mea ei alii eruditi, consul qualisque vix an, pro at deleniti vituperatoribus. No nec lorem patrioque, mel ea agam mucius alienum.</p>
+                    <span>Read more</span>
+                </div>
+            </div>
+            <div class="post-block">
+                <div class="post-content">
+                    <h4>Post Title</h4>
+                    <p>Lorem ipsum dolor sit amet, facilis volutpat ex est, te probatus adipisci eum. Epicuri antiopam mei an. Mea ei alii eruditi, consul qualisque vix an, pro at deleniti vituperatoribus. No nec lorem patrioque, mel ea agam mucius alienum.</p>
+                    <span>Read more</span>
+                </div>
+            </div>
+            <div class="post-block">
+                <div class="post-content">
+                    <h4>Post Title</h4>
+                    <p>Lorem ipsum dolor sit amet, facilis volutpat ex est, te probatus adipisci eum. Epicuri antiopam mei an. Mea ei alii eruditi, consul qualisque vix an, pro at deleniti vituperatoribus. No nec lorem patrioque, mel ea agam mucius alienum.</p>
+                    <span>Read more</span>
+                </div>
+            </div>
+        </div>
+        <div class="col m4 s12">
+            sidebar
         </div>
     </div>
 @endsection
