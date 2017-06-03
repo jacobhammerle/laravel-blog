09e50712db0a8ae9162b91b8e04f77137d7e1661
Jacob Hammerle <hammerja@miamioh.edu>
2017-06-03 11:59:37 -0400

    added contact form

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,9 +1,24 @@
 @extends('main')
 
 @section('content')
+    <h3>Contact Me.</h3>
     <div class="row">
-        <div class="col s12">
-            Contact.
+        <div class="input-field col s12">
+            <input id="email" type="email" class="validate">
+            <label for="email">Email</label>
         </div>
     </div>
+    <div class="row">
+        <div class="input-field col s12">
+            <input id="email" type="email" class="validate">
+            <label for="email">Subject</label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="input-field col s12">
+            <input id="email" type="email" class="validate">
+            <label for="email">Message</label>
+        </div>
+    </div>
+    <a class="waves-effect waves-light btn">Send</a>
 @endsection
