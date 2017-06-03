7db0f9dcdb984d8c4221b0c289ad53e4e212ab22
Jacob Hammerle <hammerja@miamioh.edu>
2017-06-02 20:15:34 -0400

    added footer

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -18,6 +18,8 @@
     </head>
     <body>
 
+        <div id="site-container">
+
         <!-- NAVBAR -->
         <nav>
             <div class="nav-wrapper">
@@ -58,6 +60,23 @@
         <div id="main-wrapper">
             
             @yield('content')
+
+        </div>
+
+        <div class="footer">
+            <div class="footer-wrapper">
+                <div class="footer-left">
+                    &copy; 2017 Jacob Hammerle
+                </div>
+                <div class="footer-right">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
 
         </div>
 
