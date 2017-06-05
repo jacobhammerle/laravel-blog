5edc2b87bf3a45ee44acd17ce033c4cc332d53f4
Jacob Hammerle <hammerja@miamioh.edu>
2017-06-04 23:27:32 -0400

    added JS partial and created Post Model

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -22,10 +22,8 @@
         <!-- FOOTER -->
         @include('partials._footer')
 
-      <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
-      <script src="/js/site.js"></script>
+        <!-- JAVASCRIPT -->
+        @include('partials._javascript')
 
     </body>
 </html>
