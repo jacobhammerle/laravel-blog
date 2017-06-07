b7605dad4d05c3518e03265625563c14bfc09d20
Jacob Hammerle <hammerja@miamioh.edu>
2017-06-06 23:37:44 -0400

    Styled "new post" modal

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -27,10 +27,8 @@
             <div class="modal-content">
                 <h5>Compose new post</h5>
                 <input class="new-post-title" type="text" name="title" placeholder="Title goes here" >
-                <textarea class="new-post-body" rows="4" placeholder="What's happening?" cols="100"></textarea>
-            </div>
-            <div class="modal-footer">
-              <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                <textarea class="new-post-body" rows="4" placeholder="Body text goes here" cols="100"></textarea>
+                <a href="#!" class="create-post-btn modal-action modal-close waves-effect waves-green btn-flat">Create Post</a>
             </div>
         </div>
 
